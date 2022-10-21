c9a3964c85e859e6bf95129fa3dfa718de6d31a5
sisatvit <sisat.vit@gmail.com>
2022-10-21 11:06:49 +0700

    change v2
    555

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("my name is vit");
+        System.out.println("555555");
     }
 }
